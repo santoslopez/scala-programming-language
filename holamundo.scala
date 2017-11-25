e23b1e2a4a10e2661a90f83fa91c44501ef7344f
Santos López <santos.lopez@galileo.edu>
2017-11-25 01:32:02 -0600

    Ejemplo de hola mundo en Scala (programación)

---
+++ holamundo.scala
@@ -0,0 +1,6 @@
+//el nombre holamundo debe ser ademas el mismo nombre de nuestro archivo.scala
+object holamundo{
+  def main(args: Array[String]){
+    println("Hola mundo!!!!! 2018 se acerca");//vamos a imprimir esto
+  }
+}
